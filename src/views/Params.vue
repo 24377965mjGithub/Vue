fdfa860a5f2eb5e79d74227aed384f72359d52ed
Mark Jason Espelita <mjespelita.swashed@gmail.com>
2024-06-20 10:54:50 +0800

    myfirstvue

---
+++ src/views/Params.vue
@@ -0,0 +1,16 @@
+<template>
+    {{ id }}
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                id: this.$route.params.id
+            }
+        },
+        mounted() {
+            
+        }
+    }
+</script>
---
+++ src/views/Welcome.vue
@@ -0,0 +1,20 @@
+<script>
+
+    import TheWelcome from '../components/TheWelcome.vue';
+    import ComponentSample from '../components/ComponentSample.vue';
+
+    export default {
+        components: {
+            TheWelcome,
+            ComponentSample
+        },
+        mounted() {
+            console.log('Hello World');
+        }
+    }
+</script>
+
+<template>
+    <ComponentSample haha="hahaha" />
+    Hello World
+</template>
